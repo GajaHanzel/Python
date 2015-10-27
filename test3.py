8f50f1be690ff9b1c72cad8a039acc001e396b1c
GajaHanzel <gaja.hanzel@gmail.com>
2015-10-27 17:00:22 +0100

    Delete test3.py

--- test3.py
+++
@@ -1,6 +0,0 @@
-reprezentanca = ["Dragic", "Lorbek", "Zupan", "Blazic", "Prepelic", "Klobucar"]
-for igralec in reprezentanca:
-    if igralec == "Slokar" or igralec == "Zupan":
-        print igralec + ", center"
-    else:
-        print igralec + ", branilec"
