26844436fa34092c8fe5347dbaa8f709d241f4ac
GajaHanzel <gaja.hanzel@gmail.com>
2015-10-27 17:00:42 +0100

    Delete test4.py

--- test4.py
+++
@@ -1,9 +0,0 @@
-repka = {
-    "Dragic": "branilec",
-    "Slokar": "center",
-    "Zupan": "center",
-    "Blazic": "branilec"
-}
-
-for igralec in repka:
-    print igralec + ", " + repka[igralec]
